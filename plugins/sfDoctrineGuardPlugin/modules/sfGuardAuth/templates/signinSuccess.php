e139bfdbf70ad368bb784b99204246c9496b914b
Emerson Estrella <emerson.estrella@gmail.com>
2012-06-07 22:56:29 -0300

    1

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/signinSuccess.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/signinSuccess.php
@@ -6,10 +6,10 @@
         <div class="span12">
         <div class="alert alert-block alert-warning fade in">
           <button class="close" data-dismiss="alert">×</button>
-          <h4 class="alert-heading">ATENÇÃO!</h4>
-          <p>O Astolfo está mudando e você pode colaborar enviando suas sugestões de melhorias.</p>
+          <h4 class="alert-heading"><?php echo __('Warning', array(), 'messages') ?>!</h4>
+          <p><?php echo sfConfig::get('app_name').__(' is changing and you can help by sending your feedback', array(), 'messages') ?>.</p>
           <p>
-            <a class="btn btn-success" href="https://spreadsheets.google.com/viewform?key=ttTqfA0XBz9_hb41cLNjwiw" target="_blank">Envie suas sugestões!</a>
+            <a class="btn btn-success" href="https://spreadsheets.google.com/viewform?key=ttTqfA0XBz9_hb41cLNjwiw" target="_blank"><?php echo __('Send your feedback', array(), 'messages') ?>!</a>
           </p>
         </div>
         </div>
@@ -19,10 +19,10 @@
       <div class="hero-unit">
         <div class="row">
         <div class="span6">
-          <h1>Astolfo</h1>
+          <h1><?php echo sfConfig::get('app_name')?></h1>
           <!-- <h4>Para acessar o Astolfo, entre com o login e senha fornecidos.</h4> -->
           <br />
-          <p>O Astolfo faz o gerenciamento de todos os web-assets da Fundação Padre Anchieta.</p>
+          <p><?php echo sfConfig::get('app_name').__(' is an open source Content Management System that allows you easily manage multiple websites at same time', array(), 'messages') ?>.</p>
           <p>Entre com suas credênciais ou peça a criação de sua conta.</p>
           <!-- <p><a class="btn btn-primary btn-large">Learn more »</a></p> -->
         </div>
@@ -34,19 +34,19 @@
       <div class="row">
         
         <div class="span4">
-          <h2>Edição de e publicação conteúdo</h2>
+          <h2><?php echo __('Editing and publishing content', array(), 'messages') ?></h2>
            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-           <p><a class="btn" href="#">Ver detalhes »</a></p>
+           <p><a class="btn" href="#"><?php echo __('Details', array(), 'messages') ?> »</a></p>
         </div>
         <div class="span4">
-          <h2>Publicação de vídeos para o Youtube</h2>
+          <h2><?php echo __('Youtube Integration', array(), 'messages') ?></h2>
            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-           <p><a class="btn" href="#">Ver detalhes »</a></p>
+           <p><a class="btn" href="#"><?php echo __('Details', array(), 'messages') ?> »</a></p>
        </div>
         <div class="span4">
-          <h2>Gerenciamento de grades e EPGs</h2>
+          <h2><?php echo __('Calendar and scheduling system', array(), 'messages') ?></h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">Ver detalhes »</a></p>
+          <p><a class="btn" href="#"><?php echo __('Details', array(), 'messages') ?> »</a></p>
         </div>
       </div>
 
