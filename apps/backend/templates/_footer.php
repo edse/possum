8ce300d797ca6eb37d0a583fdb9e945e768727f2
Emerson Estrella <emerson.estrella@gmail.com>
2012-06-07 23:01:05 -0300

    1

--- apps/backend/templates/_footer.php
+++ apps/backend/templates/_footer.php
@@ -1,6 +1,7 @@
         <hr />
         <footer>
-          <p>&copy; 2012 cmais+ O portal de conteúdo da Cultura</p>
+          <p>
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">Possum-CMS</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://possum-cms.com" property="cc:attributionName" rel="cc:attributionURL">Emerson Estrella</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+          </p>
         </footer>
-
       </div>
