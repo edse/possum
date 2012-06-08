f495adea83a0dc1fe837717f20261ea9ddacac93
Emerson Estrella <emerson.estrella@gmail.com>
2012-06-07 23:13:30 -0300

    1

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -24,6 +24,16 @@
   </style>
   
   <?php include_javascripts() ?>
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-32495519-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
 </head>
 <body>
   <?php include_partial('global/menu') ?>
